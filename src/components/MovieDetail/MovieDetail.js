d606886d26b968e7a3f11680512e8d52376c5006
zarifRahman <zarif.shahriar.rahman@gmail.com>
2022-02-06 00:56:31 +0600

    add

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -18,56 +18,63 @@ const MovieDetail = () => {
     return () => {
       dispatch(removeSeletedMovie()); //
     };
-  }, [dispatch]);
+  }, [dispatch, imdbID]);
 
   const data = useSelector(getMoviesOrshows);
-  console.log({ data });
+  console.log(Object.keys(data));
 
   return (
     <>
-      <div className="section-left">
-        <div className="movie-title">{data.Title}</div>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
-          </span>
-          <span>
-            Runtime <i className="fa fa-film"></i> : {data.Runtime}
-          </span>
-          <span>
-            Year <i className="fa fa-calendar"></i> : {data.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{data.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+      {Object.keys(data).length === 0 ? (
+        <div>Loading....</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <i className="fa fa-thumbs-up"></i> :{" "}
+                {data.imdbVotes}
+              </span>
+              <span>
+                Runtime <i className="fa fa-film"></i> : {data.Runtime}
+              </span>
+              <span>
+                Year <i className="fa fa-calendar"></i> : {data.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Generes</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Languages</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Generes</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} alt={data.Title} />
-      </div>
+        </>
+      )}
     </>
   );
 };
